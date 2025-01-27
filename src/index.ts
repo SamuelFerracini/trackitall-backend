2c020c7dfc05ef563e8288ae08faee0ae5e997c1
Samuel Ferracini <samuelferracini@Samuels-MacBook-Pro.local>
2025-01-27 15:37:24 +1030

    attempt number 123174

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { connectToDatabase } from "./database/mongo";
+
+import express from "express";
+import { registerRoutes } from "./routes";
+
+import rateLimit from "express-rate-limit";
+
+connectToDatabase();
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes in milliseconds
+  max: 5, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after later",
+  standardHeaders: true, // Include rate limit info in the response headers
+  legacyHeaders: false, // Disable X-RateLimit-* headers
+});
+
+app.get("/", (_, res) => {
+  res.send("Express Typescript on Vercel");
+});
+
+app.use(limiter);
+
+registerRoutes(app);
+
+export default app;
